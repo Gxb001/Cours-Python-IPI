e531b8f09298fb485fa7d3d10eb88d22c1bc89a2
Gab <gabfer258@gmail.com>
2024-12-20 11:47:22 +0100

    read phone ok

---
+++ Exercices/Customers.py
@@ -0,0 +1,6 @@
+Name = ("Dupont", "Jean") # Tuple
+Hobbies = ["Tennis", "Piano", "Voyages"] # List
+client =  {"Nom" : Name[0], "Prénom" : Name[1], "Age" : 25, "Email" : "sonemail@gmail.com", "Hobbies" : Hobbies} # Dictionary
+print(client["Nom"], client["Prénom"])
+
+
