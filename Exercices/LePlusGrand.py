68f3ce7a83cd5f2a004e1e7ba0bfe2677c2ba238
Gab <gabfer258@gmail.com>
2024-12-20 09:50:43 +0100

    ajout de niveaux

--- Exercices/LePlusGrand.py
+++ Exercices/LePlusGrand.py
@@ -1,6 +1,11 @@
 import random
-def guess_random_nb():
-    nb = random.randint(0, 100)
+def guess_random_nb(lvl):
+    if lvl == 1:
+        nb = random.randint(0, 10)
+    elif lvl == 2:
+        nb = random.randint(0, 100)
+    else:
+        nb = random.randint(0, 1000)
     print("Je pense à un nombre entre 0 et 100. Devinez-le !")
     while True:
         guess = int(input("Entrez un nombre: "))
@@ -11,4 +16,4 @@ def guess_random_nb():
         else:
             print("Bravo ! Vous avez trouvé le nombre.")
             break
-guess_random_nb()
+guess_random_nb(1)
